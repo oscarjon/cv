7c681fa3644e4346e27eb14529e129265bf09fef
oscarjon <oscar.jonsson@hotmail.com>
2017-04-06 20:41:34 +0200

    Add files via upload

--- js/app/models/aboutModel.js
+++ js/app/models/aboutModel.js
@@ -1,11 +1,11 @@
 define(['./basicModel'], function(BasicModel) {
 
     var text = "";
-    ServerAPI.getUser(1, function(data, status, res){
-    	text = data.responseJSON.title;
-    });
+    // ServerAPI.getUser(1, function(data, status, res){
+    // 	text = data.responseJSON.title;
+    // });
 
-    var title = ""
+    var title = "";
     // var object = {
     //  	id: 2,
     //     title: 'Oscar'
@@ -13,12 +13,30 @@ define(['./basicModel'], function(BasicModel) {
     // ServerAPI.postUser(object, function(data, status, res) {
     //     title = data.responseJSON.title;
     // });
+    
+    ServerAPI.getAllUsers(function(data, status, res){
+    	userslist = data.responseJSON;
+    	for (var i = 0; i < userslist.length; i++){
+ 			 text += userslist[i].id + " " +  userslist[i].title + "<br>";
+		}
+    });
+
 
     var model1 = new BasicModel(title, text);
     return model1;
 });
 
+function hej(model){
 
+ ServerAPI.getAllUsers(function(data, status, res){
+        model.text = "";
+        userslist = data.responseJSON;
+        for (var i = 0; i < userslist.length; i++){
+             model.text += userslist[i].id + " " +  userslist[i].title + "<br>";
+        }
+        $('.container h2').html(model.getText());
+    });
+};
 
 // function myFunction() {
 // var data;
